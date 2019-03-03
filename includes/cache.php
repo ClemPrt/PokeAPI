502f97f0ae0ce1fa0d623a936f5e01a85a9c2279
ClemPrt <clement.parot@hetic.net>
2019-03-03 18:35:56 +0100

    Add head/cache.php - bug correct

---
+++ includes/cache.php
@@ -0,0 +1,32 @@
+<?php
+// Create cache info
+    $cacheKey = md5($url);
+    $cachePath = './cache/'.$cacheKey;
+    $cacheUsed = false;
+
+
+    // Cache available
+    if(file_exists($cachePath))
+    {
+        $result = file_get_contents($cachePath);
+        $cacheUsed = true;
+    }
+
+    // Cache not available
+    else
+    {
+        // Make request to API
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+
+        $result = curl_exec($curl);
+        curl_close($curl);
+
+        // Save in cache
+        file_put_contents($cachePath, $result);
+    }
+?>
--- index.php
+++ index.php
@@ -1,72 +1,52 @@
 <?php
-$curl = curl_init();
-$base = "https://pokeapi.co/api/v2/pokemon/";
+    $curl = curl_init();
+    $url = "https://pokeapi.co/api/v2/pokemon/";
 
 
+    include 'includes/cache.php';
 
-curl_setopt_array($curl, array(
-    CURLOPT_URL => $base,
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => "",
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 30,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => "GET",
-    CURLOPT_POSTFIELDS => "{}",
-));
-
-$response = curl_exec($curl);
-$err = curl_error($curl);
-
-curl_close($curl);
-
-//if ($err) {
-//  echo "cURL Error #:" . $err;
-//} else {
-//  echo $response;
-//}
-
-    // Define city and use Paris as default
+    // Define id and use bulbasaur as default
     $id = empty($_GET['id']) ? '1' : $_GET['id'];
 
     //$id = rand(1,800);
-    $data = file_get_contents($base.$id.'/');
+    $data = file_get_contents($url.$id.'/');
     $result = json_decode($data);
     //echo $result->name."<br>";
+
+    include 'includes/head.php';
 ?>
 
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,900" rel="stylesheet">
-    <link rel="stylesheet" href="styles/style.css">
-    <title>Pokemon list</title>
-</head>
 <body>
     <div class="menu">
         <div class="inner">
             <h1>Pokémon list</h1>
-            <h3>
-                
-            </h3>
         </div>
     </div>
-        <?php
+        <?php if($result){
         $img = $result->sprites->front_default;
         $imageData = base64_encode(file_get_contents($img));
         echo '<img src="data:image/png;base64,'.$imageData.'">'.'<br>';
+        } else { ?>
+            <p>Oh nooooo ! Pokemon not found ! Retry</p>
+        <?php
+        }
         ?>
-        <h1><?= $result->name ?></h1>
-        <p>No.<?= $result->order ?></p>
+ 
+        <h1>
+            <?= $result->name; ?>
+        </h1>
+        <p>
+            <?php if ($result) {
+                    echo 'No.'.$result->order;
+                }
+            ?>
+        </p>
         <div class="button">
         <?php
             if(($result->types[0]->type->name) == true){ ?>
-                <p class="button_type"><?= $result->types[0]->type->name ?></p>
+                    <p class="button_type"><?= $result->types[0]->type->name ?></p>
         <?php
             }
         ?>
@@ -76,22 +56,28 @@ curl_close($curl);
         <?php
             }
         ?>
-        <p>Speed: <?= $result->stats[0]->base_stat ?></p>
-        <p>Special defense: <?= $result->stats[1]->base_stat ?></p>
-        <p>Special attack: <?= $result->stats[2]->base_stat ?></p>
-        <p>Defense: <?= $result->stats[3]->base_stat ?></p>
-        <p>Attack: <?= $result->stats[4]->base_stat ?></p>
-        <p>HP: <?= $result->stats[5]->base_stat ?></p>
         </div>
+        <p>
+            <?php if ($result) {
+                    echo 'Height: '.$result->height/10;
+                }
+            ?>
+        </p>
+        <p>
+            <?php if ($result) {
+                    echo 'Speed: '.$result->stats[0]->base_stat.'<br>';
+                    echo 'Special defense: '.$result->stats[1]->base_stat.'<br>';
+                    echo 'Special attack: '.$result->stats[2]->base_stat.'<br>';
+                    echo 'Defense: '.$result->stats[3]->base_stat.'<br>';
+                    echo 'Attack: '.$result->stats[4]->base_stat.'<br>';
+                    echo 'HP: '.$result->stats[5]->base_stat;
+                }
+            ?>
+        </p>
         <form action="#" method="get">
             <input type="text" name="id" placeholder="Pokemon or ID">
             <input type="submit">
         </form>
-        <?php
-            if(($id) > 807){
-                echo "ohhh no ! Pokemon not found";
-            };
-        ?>
     </div>
     <script src="scripts/app.js"></script>
 </body>
